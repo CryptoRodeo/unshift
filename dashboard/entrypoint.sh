d088985caf54b0b0504a855726a8f10f37a03c1d
Bryan Ramos <bramos@redhat.com>
2026-03-27 12:38:47 -0400

    Add fix for custom CA issue
    Allows creating PRs to deployed Gitlab instances.
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- dashboard/entrypoint.sh
+++ dashboard/entrypoint.sh
@@ -13,6 +13,12 @@ fi
 # Mark workspace directories as safe for git
 git config --global --add safe.directory '*'
 
+# Configure custom CA certificates for self-hosted git instances
+if [[ -f /app/custom-ca.crt ]]; then
+  export GIT_SSL_CAINFO=/app/custom-ca.crt
+  export NODE_EXTRA_CA_CERTS=/app/custom-ca.crt
+fi
+
 # Configure git credential helpers for token-based HTTPS auth
 if [[ -n "${GH_TOKEN:-}" ]]; then
   git config --global url."https://${GH_TOKEN}@github.com/".insteadOf "git@github.com:"
